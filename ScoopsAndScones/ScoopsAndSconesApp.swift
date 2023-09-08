855d202598b6d82c872149116e0ea0d29c6d441b
Vladimir Fibe <motiw@icloud.com>
2023-09-09 02:23:20 +0600

    Initial Commit

---
+++ ScoopsAndScones/ScoopsAndSconesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoopsAndSconesApp.swift
+//  ScoopsAndScones
+//
+//  Created by Vladimir Fibe on 09.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScoopsAndSconesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
